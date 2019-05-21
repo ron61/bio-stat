b56ccf4d3a062962fc61f620a0e019a0e0176459
Ryu Kudoh <ron233f@gmail.com>
2019-05-21 14:33:42 +0900

    report3修正done

--- 3/report3.c
+++ 3/report3.c
@@ -4,29 +4,32 @@
 #include<time.h>
 #include<math.h>
 
-double ber_05(int k,int n)
-{
-    double x = 0;
-    double r = 1;
-
-    for (int i = 0; i < k; i++)
+int factorial(int n) {
+    if (n > 0)
     {
-        r = r*(n/k);
-        n --;
-        k --;
+        return n * factorial(n - 1);
     }
+    else
+    {
+        return 1;
+    }
+}
 
-    x = r * pow(0.5,10);
+double ber_05(int k,int n) {
+    double p = pow(0.5,10);
+    double x = 0;
+    x = (factorial(n) * p) / (factorial(k)*factorial(n-k));
     return x;
 }
 
-int main(void)
-{
-    int x;
+int main(void) {
+    int x = 0;
+    double b;
     for (int i = 0; i < 11; i++)
     {
-        x = ber_05(i,10)*1000;
-        printf("%d : %d\n",i,x);
+        b = ber_05(i,10);
+        x = b * 1000;
+        printf("%d :%d\n",i,x);
     }
 
     return 0;
